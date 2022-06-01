366c6b9451f98bd38987c7bd873c264cbb83fe17
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-01 21:26:25 +0100

    updated my testing.js file

--- testing.js
+++ testing.js
@@ -1,10 +1,13 @@
-const uid = () => {
-    const head = Date.now().toString(36);
-    const tail = Math.random().toString(36).substr(2);
-    return `TN-${head}${tail}`
-}
+// const uid = () => {
+//     const head = Date.now().toString(36);
+//     const tail = Math.random().toString(36).substr(2);
+//     return `TN-${head}${tail}`
+// }
 
-console.log(uid())
+// console.log(uid())
+
+
+
 
 // Using the two pointer approach to reverse an array
 const solution = () => {
@@ -36,14 +39,16 @@ const swap = (newArray, startIndex, endIndex) => {
 }
 console.log(solution())
 
+
+
 // PALINDROME
-const palindrome = (str) => {
+// const palindrome = (str) => {
     
-    const originalString = str
-    const reversedString = originalString.split('').reverse().join('');
-    {originalString === reversedString ? console.log('true') : console.log('false')}
-}
-palindrome('madam');
+//     const originalString = str
+//     const reversedString = originalString.split('').reverse().join('');
+//     {originalString === reversedString ? console.log('true') : console.log('false')}
+// }
+// palindrome('madam');
 
 // let i = 0
 // while(i <= 20){
@@ -54,9 +59,76 @@ palindrome('madam');
 //     i++;
 // }
 
-// let lyricsNum = 99;
-// while(lyricsNum > 0){
-//     if(lyricsNum == 1) console.log(lyricsNum + " bottle of juice on the wall! " + lyricsNum + " bottle of juice! Take one down, pass it around...")
-//     console.log(lyricsNum + " bottles of juice on the wall! " + lyricsNum + " bottles of juice! Take one down, pass it around...")
-//     lyricsNum--
+
+// let sound = "" ; 
+// function laugh(num) {
+//     for (let x = 0 ; x < num ; x++) {
+//         sound = sound + "ha" ;
+//     }
+//     sound = sound +"!"; 
+//     return sound; 
 // }
+
+
+// console.log(laugh(0)) 
+// console.log(laugh(3)) 
+// console.log(laugh(4)) 
+// console.log(laugh(8)) 
+
+
+// function makeTriangle(num){
+//     let lineBreak = "";
+//     for(let i=0; i<num; i++){
+//         console.log(lineBreak += "* ")
+//    }
+// }
+
+// makeTriangle(3)
+
+
+
+// const obj = {
+//     name: "Frank",
+//     tall: true
+// }
+
+// delete obj.name;
+
+// console.log(obj.name)
+
+
+// function each(array, callback) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (callback(array[i])) {
+//       console.log(array[i]);
+//     }
+//   }
+// }
+
+// function isPositive(n) {
+//   return n > 0;
+// };
+
+// each([-2, 7, 11, -4, -10], isPositive);
+
+// function oddNums(n){
+//     debugger
+//     if(n % 2 === 0){
+//         console.log(n)
+//     }
+// }
+
+// [2,4,5,2,3,6,7,-90,6,4].forEach(oddNums)
+
+
+// const musicData = [
+//     { artist: 'Rihanna', name: 'Anti', sales: 603000 },
+//     { artist: 'Justin Bieber', name: 'Purpose', sales: 554000 }
+// ];
+
+// const albumSalesStrings = musicData.map(function(currentValue){
+//     return `${currentValue.name} by ${currentValue.artist} sold ${currentValue.sales} copies`
+// });
+// console.log(albumSalesStrings);
+
+
